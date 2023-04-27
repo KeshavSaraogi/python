3f03bb3d026a4522d6b2e74af39ee6bfa82ed5ca
ksaraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-04-27 16:24:12 -0400

    dayOne
    -- string input from user

---
+++ dayOne.py
@@ -0,0 +1,10 @@
+print("Welcome To The Band Name Generator! ")
+
+city = input("What Is The Name Of The City You Grew Up In? ")
+print(city)
+
+pet = input("What Is The Name of Your Pet? ")
+print(pet)
+
+bandName = str(city + " " + pet)
+print("Your Band Name Could Be " + bandName)
